841480e1eb66de0918571afa2a3bb92d38719002
sameer kushwaha <sameerkushwaha1130k@gmail.com>
2026-05-29 11:48:25 +0530

    Update check palindrome.c

--- Assignment_Day2/check palindrome.c
+++ Assignment_Day2/check palindrome.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+int main() {
+    int num, original, reverse = 0, remainder;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    original = num;
+
+   
+    while (num != 0) {
+        remainder = num % 10;
+        reverse = reverse * 10 + remainder;
+        num = num / 10;
+    }
+
+
+    if (original == reverse)
+        printf("%d is a palindrome number.\n", original);
+    else
+        printf("%d is not a palindrome number.\n", original);
+
+    return 0;
+}
