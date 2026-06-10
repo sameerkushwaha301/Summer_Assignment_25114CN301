985005d9b61a331638585b568f2b0e5ffc2bdd9d
sameer kushwaha <sameerkushwaha1130k@gmail.com>
2026-06-10 13:39:14 +0530

    Create Number Pyramid.c

---
+++ Assignment_Day10/Number Pyramid.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int i, j;
+
+    int rows = 5;
+
+    for(i = 1; i <= rows; i++) {
+        // Print spaces
+        for(j = 1; j <= rows - i; j++) {
+            printf(" ");
+        }
+
+        // Print increasing numbers
+        for(j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+
+        // Print decreasing numbers
+        for(j = i - 1; j >= 1; j--) {
+            printf("%d", j);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
