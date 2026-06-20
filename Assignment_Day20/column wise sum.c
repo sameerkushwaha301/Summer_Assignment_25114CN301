7d6e61397ce3da0dcc743514f88eedb86b9b4352
sameer kushwaha <sameerkushwaha1130k@gmail.com>
2026-06-20 07:22:29 +0530

    Create column wise sum.c

---
+++ Assignment_Day20/column wise sum.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int a[10][10], r, c, i, j, sum;
+    scanf("%d%d", &r, &c);
+    for(i = 0; i < r; i++)
+        for(j = 0; j < c; j++)
+            scanf("%d", &a[i][j]);
+    // Column-wise sum
+    for(j = 0; j < c; j++) {
+        sum = 0;
+        for(i = 0; i < r; i++)
+            sum += a[i][j];
+        printf("Col %d = %d\n", j+1, sum);
+    }
+    return 0;
+}
