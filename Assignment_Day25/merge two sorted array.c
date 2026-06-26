af985a20166cbe44491b54e97f992b421e987636
sameer kushwaha <sameerkushwaha1130k@gmail.com>
2026-06-26 21:34:52 +0530

    Create merge two sorted array.c

---
+++ Assignment_Day25/merge two sorted array.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main() {
+    int a[50], b[50], c[100];
+    int n1, n2, i, j, k = 0;
+    printf("Enter size of first array: ");
+    scanf("%d", &n1);
+    printf("Enter sorted elements of first array:\n");
+    for(i = 0; i < n1; i++)
+        scanf("%d", &a[i]);
+    printf("Enter size of second array: ");
+    scanf("%d", &n2);
+    printf("Enter sorted elements of second array:\n");
+    for(i = 0; i < n2; i++)
+        scanf("%d", &b[i]);
+    i = 0;
+    j = 0;
+    // Merge both arrays
+    while(i < n1 && j < n2) {
+        if(a[i] < b[j])
+            c[k++] = a[i++];
+        else
+            c[k++] = b[j++];
+    }
+    // Copy remaining elements
+    while(i < n1)
+        c[k++] = a[i++];
+    while(j < n2)
+        c[k++] = b[j++];
+    printf("Merged array:\n");
+    for(i = 0; i < k; i++)
+        printf("%d ", c[i]);
+    return 0;
+}
