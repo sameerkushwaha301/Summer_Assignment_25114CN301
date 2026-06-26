9496743280baa988f4fc6d3b2fc4c66a75053b58
sameer kushwaha <sameerkushwaha1130k@gmail.com>
2026-06-26 21:36:20 +0530

    Create common character two strings.c

---
+++ Assignment_Day25/common character two strings.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char str1[100], str2[100];
+    int i, j;
+    printf("Enter first string: ");
+    gets(str1);
+    printf("Enter second string: ");
+    gets(str2);
+    printf("Common characters: ");
+    // Compare each character of first string with second string
+    for(i = 0; str1[i] != '\0'; i++) {
+        for(j = 0; str2[j] != '\0'; j++) {
+            if(str1[i] == str2[j]) {
+                printf("%c ", str1[i]);
+                break;
+            } } }
+    return 0;
+}
